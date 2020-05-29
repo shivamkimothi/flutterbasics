bc654cef605806b7c04c0ee7d3667387a4d2080b
Shivam Kimothi <shivamkimothi@gmail.com>
2020-05-29 10:18:07 +0530

    updated ui, added a different page for liked wordpairs

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      color: Colors.white,
+      theme: ThemeData(
+          brightness: Brightness.light, primaryColor: Colors.blueGrey[900]),
       title: 'StartUp Name Generator',
       home: RandomWords(),
     );
@@ -18,14 +21,18 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  //final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider();
+        itemBuilder: /*1*/ (BuildContext context, int i) {
+          if (i.isOdd)
+            return Divider(
+              color: Colors.white,
+            );
 
           final index = i ~/ 2;
           if (index >= _suggestions.length) {
@@ -36,10 +43,21 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : Colors.white,
+      ),
+      onTap: () {
+        if (alreadySaved)
+          _saved.remove(pair);
+        else
+          _saved.add(pair);
+      },
       title: Text(
         pair.asPascalCase,
-        style: _biggerFont,
+        style: TextStyle(color: Colors.white, fontSize: 18),
       ),
     );
   }
@@ -48,10 +66,39 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
         title: Text('StartUp Name Generator'),
       ),
       body: _buildSuggestions(),
+      backgroundColor: Colors.black,
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _saved.map((WordPair pair) {
+        return ListTile(
+            title: Text(
+          pair.asPascalCase,
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ));
+      });
+      final List<Widget> divided = ListTile.divideTiles(
+        color: Colors.white,
+        tiles: tiles,
+        context: context,
+      ).toList();
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Saved Suggestions"),
+        ),
+        backgroundColor: Colors.black,
+        body: ListView(children: divided),
       );
+    }));
   }
 }
 
